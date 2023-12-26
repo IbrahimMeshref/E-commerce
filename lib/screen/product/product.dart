46cb1942c7e4038d9e2f8b4e4362ae3b7631f3a2
Ibrahim Meshref <brahimmohamedmeshref@gmail.com>
2023-12-26 13:58:14 +0200

    Update

--- lib/screen/product/product.dart
+++ lib/screen/product/product.dart
@@ -1,7 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:shoe/screen/cart/cubitaddcart/addcart_cubit.dart';
+import 'package:shoe/screen/cart/cubitgetcaet/getcart_cubit.dart';
 import 'package:shoe/screen/product/DetailsProduct.dart';
 import 'package:shoe/screen/product/cubit/product_cubit.dart';
 import 'package:shoe/screen/product/model/Productmodel.dart';
@@ -16,16 +19,18 @@ class Product extends StatefulWidget {
 }
 
 class _ProductState extends State<Product> {
-
-@override
+  @override
   void initState() {
-  context.read<ProductCubit>().getproduct();
+    context.read<ProductCubit>().getproduct();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    Color colorc1 = Colors.white;
+    Color color2 = Colors.black;
 
     return Scaffold(
       body: Padding(
@@ -40,7 +45,6 @@ class _ProductState extends State<Product> {
             child: IconButton(
               onPressed: () {
                 Navigator.pop(context);
-
               },
               icon: Icon(
                 Icons.chevron_left,
@@ -49,7 +53,6 @@ class _ProductState extends State<Product> {
               color: Colors.black,
             ),
           ),
-
           SizedBox(
             height: height * 0.02,
           ),
@@ -57,11 +60,8 @@ class _ProductState extends State<Product> {
             '${Datasend.namecategory} ',
             style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
           ),
-
           BlocBuilder<ProductCubit, ProductState>(
             builder: (context, state) {
-
-
               if (state is ProductLoading) {
                 return Center(
                     child: CircularProgressIndicator(
@@ -76,7 +76,7 @@ class _ProductState extends State<Product> {
                   child: GridView.builder(
                       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                           maxCrossAxisExtent: width * 0.7,
-                          mainAxisExtent: height * 0.47,
+                          mainAxisExtent: height * 0.5,
                           childAspectRatio: 3 / 2,
                           crossAxisSpacing: 15,
                           mainAxisSpacing: 15),
@@ -90,53 +90,132 @@ class _ProductState extends State<Product> {
                                 borderRadius: BorderRadius.circular(15)),
                             child: Padding(
                               padding: EdgeInsets.all(2.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                              child: Stack(
                                 children: [
-                                  Row(
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
-                                      CachedNetworkImage(
-                                        imageUrl: '${dproduct?[index].image}',
-                                        fit: BoxFit.fill,
-                                        width: width * 0.4,
-                                        height: height * 0.4,
-                                        placeholder: (context, url) => Center(
-                                            child: CircularProgressIndicator(
-                                          color: Color.fromARGB(255, 74, 84, 176),
-                                        )),
-                                        errorWidget: (context, url, error) =>
-                                            Icon(Icons.error),
-                                      )
+                                      SizedBox(
+                                        height: height * 0.07,
+                                      ),
+                                      Row(
+                                        children: [
+                                          CachedNetworkImage(
+                                            imageUrl:
+                                                '${dproduct?[index].image}',
+                                            fit: BoxFit.fill,
+                                            width: width * 0.4,
+                                            height: height * 0.3,
+                                            placeholder: (context, url) =>
+                                                Center(
+                                                    child:
+                                                        CircularProgressIndicator(
+                                              color: Color.fromARGB(
+                                                  255, 74, 84, 176),
+                                            )),
+                                            errorWidget:
+                                                (context, url, error) =>
+                                                    Icon(Icons.error),
+                                          )
+                                        ],
+                                      ),
+                                      Spacer(),
+                                      Text(
+                                        '${dproduct?[index].name}',
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(fontSize: 17),
+                                      ),
+                                      RichText(
+                                          text: TextSpan(
+                                              text: ' EGP ',
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 12),
+                                              children: [
+                                            TextSpan(
+                                              text: '${dproduct?[index].price}',
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 17,
+                                                  fontWeight: FontWeight.bold),
+                                            )
+                                          ])),
                                     ],
                                   ),
-                                  Spacer(),
-                                  Text(
-                                    '${dproduct?[index].name}',
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(fontSize: 17),
-                                  ),
-                                  RichText(
-                                      text: TextSpan(
-                                          text: ' EGP ',
-                                          style: TextStyle(
-                                              color: Colors.black, fontSize: 12),
-                                          children: [
-                                        TextSpan(
-                                          text: '${dproduct?[index].price}',
-                                          style: TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 17,
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      ])),
+                                  BlocBuilder<AddcartCubit, AddcartState>(
+                                    builder: (context2, state2) {
+                                      if(state2 is AddcartLoading || state2 is ProductLoading ){
+                                        return Center(
+                                            child: CircularProgressIndicator(
+                                              color: Color.fromARGB(255, 74, 84, 176),
+                                            ));
+                                      }
+                                      return Column(
+                                        children: [
+                                          Padding(
+                                            padding: EdgeInsets.only(
+                                                left: width * 0.32,
+                                                top: height * 0.01),
+                                            child: CircleAvatar(
+                                              backgroundColor: Colors.white,
+                                              child: IconButton(
+                                                  onPressed: () {},
+                                                  icon: Icon(
+                                                    Icons.favorite_border,
+                                                    color: Colors.black,
+                                                  )),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.only(
+                                                left: width * 0.32,
+                                                top: height * 0.33),
+                                            child: CircleAvatar(
+                                              backgroundColor:
+                                                  dproduct?[index].inCart ==
+                                                          true
+                                                      ? Color.fromARGB(
+                                                          255, 74, 84, 176)
+                                                      : Colors.white,
+                                              child: IconButton(
+                                                  onPressed: () {
+                                                    context
+                                                        .read<ProductCubit>()
+                                                        .getproduct();
+                                                    context2
+                                                        .read<AddcartCubit>()
+                                                        .addcart(
+                                                            idnumber:
+                                                                dproduct?[index]
+                                                                    .id as int);
+                                                    context
+                                                        .read<ProductCubit>()
+                                                        .getproduct();
 
+                                                  },
+                                                  icon: Icon(
+                                                    Icons
+                                                        .add_shopping_cart_outlined,
+                                                    color: dproduct?[index]
+                                                                .inCart ==
+                                                            true
+                                                        ? Colors.white
+                                                        : Colors.black,
+                                                  )),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  )
                                 ],
                               ),
                             ),
                           ),
                           onTap: () {
-                            Datasend.idproduct=index;
-                            Datasend.idddproduct=dproduct?[index].id as int;
+                            Datasend.idproduct = index;
+                            Datasend.idddproduct = dproduct?[index].id as int;
 
                             Get.to(DetailsProduct());
                           },
